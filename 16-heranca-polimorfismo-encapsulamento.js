efbdae40fab82643ade1c8ccc7572555491caf53
Matheo Acosta <matheo200817@gmail.com>
2024-09-25 07:47:54 -0400

    add modificado objeto

--- 16-heranca-polimorfismo-encapsulamento.js
+++ 16-heranca-polimorfismo-encapsulamento.js
@@ -141,3 +141,22 @@ const mecanico = new Mecanico("Roberto das Engrenagens", 8, 90, 40, 5);
 //Chamando os métodos
 mecanico.receberDano(30); // Método sobrescrito
 mecanico.contruirTorre(); // Método especifico
+
+console.log("----------------------------------------------------------------")
+console.log("Modificando os objetos usando setters:");
+console.log("----------------------------------------------------------------");
+
+//Modificando o objeto assassno usando setters
+assassino.nome = "Raven";
+console.log(`Nome modificado: ${assassino.nome}`);
+assassino.atacar();
+
+//Modificando o objeto paladino usando setters
+paladino.vida = 150;
+console.log(`Vida modificada: ${paladino.vida}`);
+paladino.defesa();
+
+//Modificando o objeto mecanico usando setters
+mecanico.mana = 60;
+console.log(`Mana modificada: ${mecanico.mana}`);
+mecanico.receberDano(50);
