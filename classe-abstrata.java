66bc9ac7b3df3e29cfc057e1611c38ad13fc1329
Matheo Acosta <matheo200817@gmail.com>
2024-09-30 09:48:17 -0400

    add classe guerreiro

--- classe-abstrata.java
+++ classe-abstrata.java
@@ -11,3 +11,25 @@ abstract class Personagem {
     public abstract void atacar();
     public abstract void defender();
 }
+// Classe derivada "Guerreiro"
+class Guerreiro extends Personagem {
+    private String arma:
+
+    // Constructor da classe Guerreiro
+    public Guerreiro(String nome, String arma) {
+        super(nome);
+        this.arma = arma;
+    }
+
+    // Sobrescrita do método Atacar da classe Abstrata Personagem
+    @Override
+    public void atacar() {
+        System.out.println(nome + " ataca com a " + arma + "!");
+    }
+
+    // Sobrescrita do método Defender da classe Abstrata Personagem
+    @Override
+    public void defender() {
+        System.out.println(nome + " levanta o escudo para se defender!");
+    }
+}
