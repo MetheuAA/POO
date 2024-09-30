3aaadffc84e03eb2ca4f0e5f3bead9c8b5b3c358
Matheo Acosta <matheo200817@gmail.com>
2024-09-30 11:04:49 -0400

    add objetos

--- 19-classe-abstrata-simulada-em-javascript.js
+++ 19-classe-abstrata-simulada-em-javascript.js
@@ -33,3 +33,33 @@ class Guerreiro extends Personagem {
         console.log(`${this.nome} levantou o escudo para se defender!`);
     }
 }
+//Classe derivada "Mago"
+class Mago extends Personagem {
+    constructor(nome, magia) {
+        super(nome);
+        this.magia = magia;
+    }
+
+    // Implementação dos métodos abstrados da classe Persongem
+    atacar() {
+        console.log(`${this.nome} lança a ${this.magia}!`);
+    }
+
+    defender() {
+        console.log(`${this.nome} usa um escudo mágico para se defender!`);
+    }
+}
+// Criando instâncias e testando o comportamento
+try {
+    const personagem = new Personagem ("invalido"); // Isso vai lançar um erro
+} catch (error) {
+    console.log(error.message); // "Não é possivel instaciar a classe abstrata Personagem diretamente."
+}
+
+const guerreiro = new Guerreiro("Thorin", "Espada");
+guerreiro.atacar(); //"Thorin ataca com a Espada!"
+guerreiro.defender(); // "Thorin levanta o escudo para se defender!"
+
+const mago = new Mago("Gandalf", "Bola de Fogo");
+mago.atacar(); // "Gandalf lança a Bola de Fogo!"
+mago.defender(); // "Gandalf usa um escudo mágico para se defender!"
